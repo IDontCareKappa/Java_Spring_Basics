748df762fbbc92170df51d357711e7d06f9e235a
Tomasz <massamo24@gmail.com>
2021-12-07 13:27:06 +0100

    Added Post, Put, Delete functions

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -2,6 +2,7 @@ package com.example.demo.student;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table
@@ -19,13 +20,15 @@ public class Student {
     private Long id;
     private String name;
     private String email;
-    private Integer age;
     private LocalDate dob;
+
+    @Transient
+    private Integer age;
 
     public Student() {
     }
 
-    public Student(Long id, String name, String email, Integer age, LocalDate dob) {
+    public Student(Long id, String name, String email, LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -33,7 +36,7 @@ public class Student {
         this.dob = dob;
     }
 
-    public Student(String name, String email, Integer age, LocalDate dob) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.age = age;
@@ -65,7 +68,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
